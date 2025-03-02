1543f23671fb499ac602ef18837218521f90257e
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-02 15:22:11 -0300

    refactor: update redirect paths to dashboard and translate reset password page to Spanish

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -10,26 +10,26 @@ export default async function ResetPassword(props: {
   const searchParams = await props.searchParams;
   return (
     <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
-      <h1 className="text-2xl font-medium">Reset password</h1>
+      <h1 className="text-2xl font-medium">Resetear password</h1>
       <p className="text-sm text-foreground/60">
-        Please enter your new password below.
+        Por favor, ingrese su nueva contraseña a continu
       </p>
-      <Label htmlFor="password">New password</Label>
+      <Label htmlFor="password">Nueva Contraseña</Label>
       <Input
         type="password"
         name="password"
-        placeholder="New password"
+        placeholder="Nueva Contraseña"
         required
       />
-      <Label htmlFor="confirmPassword">Confirm password</Label>
+      <Label htmlFor="confirmPassword">Confirmar Contraseña</Label>
       <Input
         type="password"
         name="confirmPassword"
-        placeholder="Confirm password"
+        placeholder="Confirmar Contraseña"
         required
       />
       <SubmitButton formAction={resetPasswordAction}>
-        Reset password
+        Resetear Contraseña
       </SubmitButton>
       <FormMessage message={searchParams} />
     </form>
