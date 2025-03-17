33f3af828435258804aab81d6d9c6281e60e38de
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-16 22:00:40 -0300

    Add getSpaceBySlug action and SpaceDetailsPage component for space retrieval by slug

---
+++ actions/getSpaceBySlug.ts
@@ -0,0 +1,42 @@
+"use server"
+
+import { prisma } from "@/prisma/prisma-client"
+
+export const getSpaceBySlug = async (slug: string) => {
+    try {
+
+        const space = await prisma.spaces.findFirst({
+            where: {
+                slug : slug
+            },
+            include : {
+                spaces_activation_times : true,
+            }
+        })
+
+        if (!space) {
+            throw new Error("Espacio no encontrado")
+        }
+
+        return {
+            data: space,
+            errorMessage: "",
+            hasError: false
+        }
+
+    } catch (error) {
+        if (error instanceof Error) {
+            return {
+                data: null,
+                errorMessage: error.message,
+                hasError: true
+            }
+        }
+
+        return {
+            data: null,
+            errorMessage: "Error al obtener el espacio",
+            hasError: true
+        }
+    }
+}
---
+++ app/[locale]/spaces/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { getSpaceBySlug } from "@/actions/getSpaceBySlug"
+import { redirect } from "next/navigation"
+
+type SpaceDetailsPageProps = {
+    params: Promise<{ slug: string }>,
+}
+
+async function SpaceDetailsPage({ params }: SpaceDetailsPageProps) {
+
+    const { slug } = await params
+    const { data } = await getSpaceBySlug(slug)
+
+    if (!data) {
+        redirect("/spaces")
+    }
+
+    return (
+        <section className="grow flex flex-col">
+            <div className="flex justify-between items-center mb-10">
+                <div>
+                    <h2 className="font-bold text-2xl capitalize">{data.name}</h2>
+                    <p className="text-muted-foreground">{data.subject}</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default SpaceDetailsPage
