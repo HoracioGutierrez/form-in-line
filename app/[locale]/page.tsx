20719f1705d6b6fb4da4ca6b805a99cd59852c37
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-18 20:25:07 -0300

    Update HomePage component content and styling for improved clarity and user engagement

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -8,11 +8,15 @@ async function HomePage() {
 
   return (
     <section className="font-[family-name:var(--font-geist-sans)] grow flex flex-col justify-center">
-      <div className="container mx-auto flex flex-col items-center text-center  xl:max-w-3xl">
-        <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-          <span className="text-black/20 dark:text-accent">laborum doloribus</span>delectus
+      <div className="container mx-auto flex flex-col items-center text-center xl:max-w-3xl">
+        <h1 className="text-4xl font-bold leading-none sm:text-5xl">
+          Form
+          <span className="text-black/20 dark:text-accent">-in-</span>
+          line
         </h1>
-        <p className="px-8 mt-8 mb-12 text-lg max-w-[50ch]">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+        <p className="px-8 mt-4 mb-12 text-lg max-w-[50ch]">
+          The easiest way to manage your people queues and online classes, let them join the line and wait for their turn!
+        </p>
         <div className="flex flex-wrap justify-center gap-2">
           <Button variant="outline" size="lg" asChild>
             <Link href="/signup">
