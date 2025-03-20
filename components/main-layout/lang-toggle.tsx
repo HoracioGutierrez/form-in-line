f476ec6d7247dc78966f4ad81a0e63fce93ec54b
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-19 22:06:02 -0300

    refactor: Enhance language toggle component with dropdown menu and support for Portuguese localization

--- components/main-layout/lang-toggle.tsx
+++ components/main-layout/lang-toggle.tsx
@@ -1,26 +1,51 @@
 "use client"
-import { useChangeLocale, useCurrentLocale, useI18n } from "@/locales/client"
 import { Button } from "../ui/button"
 import toast from "react-hot-toast"
-import { useTranslations } from "next-intl"
+import { useLocale, useTranslations } from "next-intl"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useParams } from "next/navigation"
+import { startTransition } from "react"
+import { usePathname, useRouter } from "@/i18n/navigation"
 
 function LangToggle() {
 
-    const changeLocale = useChangeLocale()
-    const currentLocale = useCurrentLocale()
-    //const t = useI18n()
     const t = useTranslations("layout")
+    const locale = useLocale()
+    const router = useRouter()
+    const pathname = usePathname()
+    const params = useParams()
 
 
-    const handleLocaleChange = () => {
+    const handleLocaleChange = (e: any) => {
         toast.loading(t("locale.loading"), { duration: 1000 })
-        changeLocale(currentLocale === "en" ? "es" : "en")
+        const newLocale = e.currentTarget.dataset.value
+        startTransition(() => {
+            router.replace(
+                { pathname },
+                { locale: newLocale }
+            )
+        })
     }
 
     return (
-        <Button variant="link" className="cursor-pointer p-0" onClick={handleLocaleChange}>
-            {currentLocale === "en" ? "ES" : "EN"}
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="link" className="cursor-pointer p-0" onClick={handleLocaleChange}>
+                    {locale}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={handleLocaleChange} data-value="en">
+                    EN
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={handleLocaleChange} data-value="es">
+                    ES
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={handleLocaleChange} data-value="pt">
+                    PT
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 
